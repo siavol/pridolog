2ea3b01eb2280b8a44e9e6622d69ab6630e60e9e
Ilya Shestakov <ishestakov@accusoft.com>
2018-02-03 00:17:45 +0300

    Find token by position

--- server/src/jsonParser.ts
+++ server/src/jsonParser.ts
@@ -1,10 +1,9 @@
 import * as _ from 'lodash'
 import * as tokenize from 'json-tokenize'
-import { start } from 'repl';
 
 enum TokenType {
     Whitespace = 'whitespace',
-    Punctuator = 'punctuator',
+    Punctuation = 'punctuation',
     String = 'string',
     Number = 'number',
     Literal = 'literal'
@@ -45,17 +44,71 @@ export function getToken(json: string, position: number): IJsonToken {
             return t.position.start.lineno === 1 && t.position.end.lineno === 1
                 && t.position.start.column <= position && t.position.end.column >= position;                
         } else {
-            return t.position.column < position;
+            return t.position.column <= position;
         }
     });
 
+    if (tokenIndex < 0) {
+        return null;
+    }
+
+    // console.log(tokens);
+    // console.log(tokenIndex);
+
+    const field = findField(tokens, tokenIndex);
+    const value = getValueForField(tokens, field.index);
+
     return {
-        field: tokens[tokenIndex].value,
-        path: tokens[tokenIndex].value,
-        value: tokens[tokenIndex + 3].value
+        field: field.token.value,
+        path: field.token.value,
+        value: value
     };
 }
 
 function isRange(pos: IRangePosition | ICharPosition): pos is IRangePosition {
     return _.has(pos, 'start');
+}
+
+function findField(tokens: IToken[], index: number): { index: number, token: IToken } {
+    let prev = { index, token: <IToken> null };
+    do {
+        prev = getPrevReal(tokens, prev.index);        
+    } while(prev.index > 0 
+        && !(prev.token.type === TokenType.Punctuation && prev.token.raw === '{')
+        && !(prev.token.type === TokenType.Punctuation && prev.token.raw === ','));
+    return getNextReal(tokens, prev.index);
+}
+
+function getValueForField(tokens: IToken[], fieldIndex: number): any {
+    const {index: delimiterIndex, token: delimiter} = getNextReal(tokens, fieldIndex)
+    if (delimiter.type != TokenType.Punctuation || delimiter.raw !== ':') {
+        throw new Error(`Not expected token in position ${delimiterIndex}`);
+    }
+    
+    const value = getNextReal(tokens, delimiterIndex);
+    return value.token.value;
+}
+
+function getNextReal(tokens: IToken[], index: number): { index: number, token: IToken} {
+    if (index === tokens.length-1) {
+        return {index, token: tokens[index] };
+    }
+
+    let result = index + 1;
+    while (result < tokens.length && tokens[result].type === TokenType.Whitespace) {
+        result++;
+    }
+    return { index: result, token: tokens[result] };
+}
+
+function getPrevReal(tokens: IToken[], index: number): { index: number, token: IToken } {
+    if (index === 0) {
+        return { index: 0, token: tokens[0] };
+    }
+
+    let result = index-1;
+    while (result > 0 && tokens[result].type === TokenType.Whitespace) {
+        result--;
+    }
+    return { index: result, token: tokens[result] };
 }
--- server/test/jsonParser.spec.ts
+++ server/test/jsonParser.spec.ts
@@ -15,12 +15,28 @@ describe('jsonParser', () => {
         });
 
         it('should return first level token', () => {
-            const json = '{"foo": 1}';
+            const json = '{"foo": 1, "bar": "test"}';
             expect(getToken(json, 3)).eql({
                 field: 'foo',
                 path: 'foo', 
                 value: 1
             });
+            expect(getToken(json, 8)).eql({
+                field: 'foo',
+                path: 'foo',
+                value: 1
+            });
+
+            expect(getToken(json, 17)).eql({
+                field: 'bar',
+                path: 'bar',
+                value: "test"
+            });
+        });
+
+        it('shoud return null if no token at position', () => {
+            const json = '{"foo": 1, "bar": "test"}';
+            expect(getToken(json, 0)).to.be.null;
         });
     });
 });
