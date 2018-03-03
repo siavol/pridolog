8e47c945be2a12f855c7af65432d69ba7a321793
Ilya Shestakov <ishestakov@accusoft.com>
2018-03-03 13:45:09 +0300

    Documentation update

--- client/CHANGELOG.md
+++ client/CHANGELOG.md
@@ -1,2 +1,6 @@
+# Version 0.0.2
+- CodeLens for all tasks
+- CodeLens for long operations
+
 # Version 0.0.1
 - Initial releases
--- server/test/time.spec.ts
+++ server/test/time.spec.ts
@@ -11,7 +11,7 @@ describe.only('time', () => {
         { durationInMs: 3 * 60 * 1000, expectedResult: '3 minutes' },
         { durationInMs: 3 * 60 * 1000 + 2 * 1000 + 123, expectedResult: '3 minutes 2 seconds' },
         { durationInMs: 3 * 60 * 1000 + 1 * 1000 + 123, expectedResult: '3 minutes 1 second' },
-        { durationInMs: 4 * 60 * 60 * 1000 + 2 * 1000 + 123, expectedResult: '4 hours' }
+        { durationInMs: 4 * 60 * 60 * 1000 + 2 * 1000 + 123, expectedResult: '4 hours' },
         { durationInMs: 1 * 60 * 60 * 1000 + 1 * 60 * 1000 + 123 + 10 * 1000, expectedResult: '1 hour 1 minute' }
     ].forEach(testCase => {
         it(`should format duration in ms ${testCase.durationInMs} as '${testCase.expectedResult}'`, () => {
