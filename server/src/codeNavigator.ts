850615686e92356c4686427d37d01d59f622afd3
Ilya Shestakov <ishestakov@accusoft.com>
2018-05-02 15:33:10 +0300

    Use documents cache for Find gid referencies

--- server/src/codeNavigator.ts
+++ server/src/codeNavigator.ts
@@ -35,8 +35,8 @@ export class CodeNavigator {
     public findAllEntriesForGid(gid: string): Location[] {
         const logFiles = this.documentsProvider.getDocuments();
         let result = _(logFiles)
-            .map(file => ({ uri: file, text: this.documentsProvider.getDocumentText(file) }))
-            .flatMap(file => parseTextLog(file.text).map(line => ({uri: file.uri, text:line.source, line: line.line, logItem:line.logItem})))
+            .map(file => ({ uri: file, lines: this.getLogLines(file) }))
+            .flatMap(file => file.lines.map(line => ({uri: file.uri, text:line.source, line: line.line, logItem:line.logItem})))
             .filter(item => item.logItem.gid === gid)
             .map(item => ({ uri: item.uri, range: Range.create(item.line, 0, item.line, item.text.length-1) }))
             .value();
